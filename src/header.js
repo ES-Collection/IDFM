577083b121d5e71a76e7bb034842cefd5ed53f93
Bruno Herfst <mail@brunoherfst.com>
2017-05-12 16:32:06 +1000

    update header

--- src/header.js
+++ src/header.js
@@ -1,3 +1,23 @@
+/*
+
+	  _____           _     __  __                                   
+	 |  ___|__  _ __ | |_  |  \/  | __ _ _ __   __ _  __ _  ___ _ __ 
+	 | |_ / _ \| '_ \| __| | |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '__|
+	 |  _| (_) | | | | |_  | |  | | (_| | | | | (_| | (_| |  __/ |   
+	 |_|  \___/|_| |_|\__| |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_|   
+	                                                 |___/           
+
+	  A complete package of the InDesign Font Manager (Includes ESPM)
+	  This manager can load and save InDesign Outline Fonts (IOF) 
+
+	  Bruno Herfst 2017
+	  
+	  MIT license (MIT)
+
+	  https://github.com/GitBruno/IDFM
+*/
+
 #target indesign
 
 #targetengine 'IDFM';
+
